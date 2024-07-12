327af55a805b37b50df9bcc813e2f5d36324699d
Maria <carrizomascarell@terrasysgeo.com>
2024-07-12 16:43:41 +0200

    added Plot2Datas

--- src/Plot.py
+++ src/Plot.py
@@ -98,3 +98,119 @@ def grid(model, depthmax=10, ny=101, nlay=2):
                 y1 += 1
         
     return grid
+
+def Plot2Datas(data_1D, data_3D):
+
+    fig, ax = plt.subplots(3,4, figsize=(10,5), sharex=True)
+    
+    # Quadrature 2m
+    ax[0,0].semilogy(data_1D[:,0], '.b', label='H')
+    ax[0,0].semilogy(data_3D[:,0], 'xb' )
+    ax[0,0].semilogy(data_1D[:,3], '.r', label='P')
+    ax[0,0].semilogy(data_3D[:,3], 'xr' )   
+    ax[0,0].semilogy(data_1D[:,6], '.k', label='V')
+    ax[0,0].semilogy(data_3D[:,6], 'xk' )
+    ax[0,0].legend(fontsize=7)
+    ax[0,0].tick_params( labelsize=7)
+    ax[0,0].set_title('2m Quadrature', fontsize=7)
+
+    ax[0,1].semilogy(100*np.abs((data_1D[:,0]-data_3D[:,0])/data_3D[:,0]), ':b', label='H2 Q')
+    ax[0,1].semilogy(100*np.abs((data_1D[:,3]-data_3D[:,3])/data_3D[:,3]), ':r', label='P2 Q')
+    ax[0,1].semilogy(100*np.abs((data_1D[:,6]-data_3D[:,6])/data_3D[:,6]), ':k', label='V2 Q')
+    ax[0,1].legend(fontsize=7)
+    ax[0,1].tick_params(labelsize=7)
+    ax[0,1].set_title('2mQ Rel Diff %', fontsize=7)
+    
+    # Quadrature 4m
+    ax[1,0].semilogy(data_1D[:,1], '.b', label = 'H4 Q')
+    ax[1,0].semilogy(data_3D[:,1], 'xb')
+    ax[1,0].semilogy(data_1D[:,4], '.r', label = 'P4 Q')
+    ax[1,0].semilogy(data_3D[:,4], 'xr' )
+    ax[1,0].semilogy(data_1D[:,7], '.k', label = 'V4 Q')
+    ax[1,0].semilogy(data_3D[:,7], 'xk')
+    ax[1,0].legend(fontsize=7)
+    ax[1,0].tick_params( labelsize=7)
+    ax[1,0].set_title('4m Quadrature', fontsize=7)
+
+    ax[1,1].semilogy(100*np.abs((data_1D[:,1]-data_3D[:,1])/data_3D[:,1]), ':b', label='H4 Q')
+    ax[1,1].semilogy(100*np.abs((data_1D[:,4]-data_3D[:,4])/data_3D[:,4]), ':r', label='P4 Q')
+    ax[1,1].semilogy(100*np.abs((data_1D[:,7]-data_3D[:,7])/data_3D[:,7]), ':k', label='V4 Q')
+    ax[1,1].legend(fontsize=7)
+    ax[1,1].tick_params( labelsize=7)
+    ax[1,1].set_title('4mQ Rel Diff %', fontsize=7)
+    
+    # Quadrature 8m
+    ax[2,0].semilogy(data_1D[:,2], '.b', label= 'H8 Q')
+    ax[2,0].semilogy(data_3D[:,2], 'xb' )
+    ax[2,0].semilogy(data_1D[:,5], '.r', label= 'P8 Q')
+    ax[2,0].semilogy(data_3D[:,5], 'xr')
+    ax[2,0].semilogy(data_1D[:,8], '.k', label= 'V8 Q')
+    ax[2,0].semilogy(data_3D[:,8], 'xk' )
+    ax[2,0].legend(fontsize=7)
+    ax[2,0].tick_params( labelsize=7)
+    ax[2,0].set_title('8m Quadrature', fontsize=7)
+
+    ax[2,1].semilogy(100*np.abs((data_1D[:,2]-data_3D[:,2])/data_3D[:,2]), ':b', label='H8 Q')
+    ax[2,1].semilogy(100*np.abs((data_1D[:,5]-data_3D[:,5])/data_3D[:,5]), ':r', label='P8 Q')
+    ax[2,1].semilogy(100*np.abs((data_1D[:,8]-data_3D[:,8])/data_3D[:,8]), ':k', label='V8 Q')
+    ax[2,1].legend(fontsize=7)
+    ax[2,1].tick_params(labelsize=7)
+    ax[2,1].set_title('8mQ Rel Diff %', fontsize=7)
+    
+    # In-Phase 2m
+    ax[0,2].semilogy(data_1D[:,9], '.b', label='H2 IP')
+    ax[0,2].semilogy(data_3D[:,9], 'xb' )
+    ax[0,2].semilogy(data_1D[:,12], '.r', label='P2 IP')
+    ax[0,2].semilogy(data_3D[:,12], 'xr' )
+    ax[0,2].semilogy(data_1D[:,15], '.k', label='V2 IP')
+    ax[0,2].semilogy(data_3D[:,15], 'xk' )
+    ax[0,2].legend(fontsize=7)
+    ax[0,2].tick_params( labelsize=7)
+    ax[0,2].set_title('2m In-Phase', fontsize=7)
+
+    ax[0,3].semilogy(100*np.abs((data_1D[:,9]-data_3D[:,9])/data_3D[:,9]), ':b', label = 'H2 IP')
+    ax[0,3].semilogy(100*np.abs((data_1D[:,12]-data_3D[:,12])/data_3D[:,12]), ':r', label = 'P2 IP')
+    ax[0,3].semilogy(100*np.abs((data_1D[:,15]-data_3D[:,15])/data_3D[:,15]), ':k', label='V2 IP')
+    ax[0,3].legend(fontsize=7)
+    ax[0,3].tick_params(labelsize=7)
+    ax[0,3].set_title('2mIP Rel Diff %', fontsize=7)
+    
+    # In-Phase 4m
+    ax[1,2].semilogy(data_1D[:,10], '.b', label = 'H4 IP')
+    ax[1,2].semilogy(data_3D[:,10], 'xb' )
+    ax[1,2].semilogy(data_1D[:,13], '.r', label = 'P4 IP')
+    ax[1,2].semilogy(data_3D[:,13], 'xr' )
+    ax[1,2].semilogy(data_1D[:,16], '.k', label = 'V4 IP')
+    ax[1,2].semilogy(data_3D[:,16], 'xk' )
+    ax[1,2].legend(fontsize=7)
+    ax[1,2].tick_params(labelsize=7)
+    ax[1,2].set_title('4m In-Phase', fontsize=7)
+    
+    ax[1,3].semilogy(100*np.abs((data_1D[:,10]-data_3D[:,10])/data_3D[:,10]), ':b', label = 'H4 IP')
+    ax[1,3].semilogy(100*np.abs((data_1D[:,13]-data_3D[:,13])/data_3D[:,13]), ':r', label = 'P4 IP')
+    ax[1,3].semilogy(100*np.abs((data_1D[:,16]-data_3D[:,16])/data_3D[:,16]), ':k', label='V4 IP')
+    ax[1,3].legend(fontsize=7)
+    ax[1,3].tick_params(labelsize=7)
+    ax[1,3].set_title('4mIP Rel Diff %', fontsize=7)
+    
+    # In-Phase 8m
+    ax[2,2].semilogy(data_1D[:,11], '.b', label= 'H8 IP')
+    ax[2,2].semilogy(data_3D[:,11], 'xb' )
+    ax[2,2].semilogy(data_1D[:,14], '.r', label= 'P8 IP')
+    ax[2,2].semilogy(data_3D[:,14], 'xr' )
+    ax[2,2].semilogy(data_1D[:,17], '.k', label= 'V8 IP')
+    ax[2,2].semilogy(data_3D[:,17], 'xk' )
+    ax[2,2].legend(fontsize=7)
+    ax[2,2].tick_params(labelsize=7)
+    ax[2,2].set_title('8m In-Phase', fontsize=7)
+
+    ax[2,3].semilogy(100*np.abs((data_1D[:,11]-data_3D[:,11])/data_3D[:,11]), ':b', label = 'H8 IP')
+    ax[2,3].semilogy(100*np.abs((data_1D[:,14]-data_3D[:,14])/data_3D[:,14]), ':r', label = 'P8 IP')
+    ax[2,3].semilogy(100*np.abs((data_1D[:,17]-data_3D[:,17])/data_3D[:,17]), ':k', label='V8 IP')
+    ax[2,3].legend(fontsize=7)
+    ax[2,3].tick_params(labelsize=7)
+    ax[2,3].set_title('8mIP Rel Diff %', fontsize=7)
+    
+    plt.tight_layout()
+    
+
