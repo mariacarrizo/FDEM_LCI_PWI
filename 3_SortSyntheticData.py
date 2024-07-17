5248f1e31da98e4aef6527e7853f61265a7b9a32
Maria <carrizomascarell@terrasysgeo.com>
2024-07-17 18:30:00 +0200

    updated

--- 3_SortSyntheticData.py
+++ 3_SortSyntheticData.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 # 1. Upload data
 
-DATA = np.load('data/data_s1_c2_ur.pkl', allow_pickle=True)
+DATA = np.load('data/data_s2_c1_ur.pkl', allow_pickle=True)
 
 # 2. Sort data by coil geometry
 
@@ -41,4 +41,4 @@ for p in range(-3,npos):
         print(DATA_sorted.loc[(DATA_sorted.midpx > p+.06) & (DATA_sorted.midpx <p +1.06)])
     print()
 
-np.save('data/data_s1_c2_ur_sort', DATA_sorted_midpoint)
+np.save('data/data_s2_c1_ur_sort', DATA_sorted_midpoint)
